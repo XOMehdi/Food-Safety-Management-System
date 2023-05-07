b519571ca50b0d783599e7070003f16c288f8a52
Muhamamd Mehdi <mahdikhan660@gmail.com>
2023-05-07 19:50:30 +0500

    Ingredient successfully added with some bugs

--- adding_ingredient.php
+++ adding_ingredient.php
@@ -16,29 +16,28 @@ if (isset($_GET['ingredient_submitted'])) {
     $ingredient_name = $_GET['ingredient_name'];
     $ingredient_cost = $_GET['ingredient_cost'];
     $purchase_date = $_GET['purchase_date'];
-    $expiry_date = $_GET['expire_date'];
+    $expire_date = $_GET['expire_date'];
 
     $supp_name = $_GET['supp_name'];
     $supp_phone = $_GET['supp_phone'];
     $supp_country = $_GET['supp_country'];
 
     $allergy_type = $_GET['allergy_type'];
-    $allergy_severity = $_GET['severity_level'];
-
+    $allergy_severity = $_GET['severty_level'];
 
 
     $conn->query("INSERT INTO allergy (allergy_type, allergy_severity) VALUES ('$allergy_type', '$allergy_severity')");
     $conn->query("INSERT INTO supplier (supp_name, supp_phone, supp_country) VALUES ('$supp_name', '$supp_phone', '$supp_country')");
 
-    $query = $conn->query("SELECT allergy_num FROM allergy");
+    $query = $conn->query("SELECT allergy_num FROM allergy ORDER BY allergy_num DESC LIMIT 1");
     $row = $query->fetch(PDO::FETCH_OBJ);
     $allergy_num = $row->allergy_num;
 
-    $query = $conn->query("SELECT supp_num FROM supplier");
+    $query = $conn->query("SELECT supp_num FROM supplier ORDER BY supp_num DESC LIMIT 1");
     $row = $query->fetch(PDO::FETCH_OBJ);
     $supp_num = $row->supp_num;
 
-    $conn->query("INSERT INTO THEMEPARK (ingredient_id, ingredient_name, ingredient_cost, purchase_date, expire_date, allergy_type, supplier) VALUES ('$ingredient_id', '$ingredient_name', '$ingredient_cost', '$purchase_date', '$expire_date', '$allergy_num', '$supp_num')");
+    $conn->query("INSERT INTO ingredient (ingredient_id, ingredient_name, ingredient_cost, purchase_date, expire_date, allergy_type, supplier) VALUES ('$ingredient_id', '$ingredient_name', '$ingredient_cost', '$purchase_date', '$expire_date', '$allergy_num', '$supp_num')");
 
     echo "Your form has been submitted\n";
 } else {

3aea0124cc94fec52a6ef419c97e797efcd980e7
Muhamamd Mehdi <mahdikhan660@gmail.com>
2023-05-07 20:37:06 +0500

    Navigation buttons & events working

--- adding_ingredient.js
+++ adding_ingredient.js
@@ -1,32 +1,54 @@
 const moveToIngredient = document.querySelectorAll(".move-to-ingredient");
 const moveToSupplier = document.querySelectorAll(".move-to-supplier");
 const moveToAllergy = document.querySelectorAll(".move-to-allergy");
-const submitIngredientButton = document.getElementById(
-  "submit-ingredient-button"
-);
+// const submitIngredientButton = document.getElementById(
+//   "submit-ingredient-button"
+// );
 
 let default_class =
   "move-to-supplier px-6 py-2 rounded-lg text-gray-700 hover:text-gray-500 focus:outline-none";
 
-moveToIngredient.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    e.target.classList.add("bg-white, mr-2");
-    moveToSupplier.classList = default_class;
-    moveToAllergy.classList = default_class;
+moveToIngredient.forEach((ingredientButoon) => {
+  ingredientButoon.addEventListener("click", (e) => {
+    e.target.classList.add("bg-white");
+    e.target.classList.add("mr-2");
+
+    moveToSupplier.forEach((supplierButton) => {
+      supplierButton.classList = default_class;
+    });
+
+    moveToAllergy.forEach((allergyButton) => {
+      allergyButton.classList = default_class;
+    });
   });
 });
 
-moveToSupplier.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    e.target.classList.add("bg-white, mr-2");
-    //   let def = moveToIngredient.className.replace("bg-white mr-2", "");
-    moveToIngredient.classList = default_class;
-    moveToAllergy.classList = default_class;
+moveToSupplier.forEach((supplierButton) => {
+  supplierButton.addEventListener("click", (e) => {
+    e.target.classList.add("bg-white");
+    e.target.classList.add("mr-2");
+
+    moveToIngredient.forEach((ingredientButoon) => {
+      ingredientButoon.classList = default_class;
+    });
+
+    moveToAllergy.forEach((allergyButton) => {
+      allergyButton.classList = default_class;
+    });
   });
 });
 
-moveToAllergy.addEventListener("click", (e) => {
-  e.target.classList.add("bg-white mr-2");
-  moveToIngredient.classList = default_class;
-  moveToSupplier.classList = default_class;
+moveToAllergy.forEach((allergyButton) => {
+  allergyButton.addEventListener("click", (e) => {
+    e.target.classList.add("bg-white");
+    e.target.classList.add("mr-2");
+
+    moveToIngredient.forEach((ingredientButoon) => {
+      ingredientButoon.classList = default_class;
+    });
+
+    moveToSupplier.forEach((supplierButton) => {
+      supplierButton.classList = default_class;
+    });
+  });
 });

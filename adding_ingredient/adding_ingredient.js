8e2f75c2f813a4ddafab07c8a3b15c45d62df052
Muhamamd Mehdi <mahdikhan660@gmail.com>
2023-05-11 20:04:35 +0500

    Refactored adding_ingredient section

--- adding_ingredient/adding_ingredient.js
+++ adding_ingredient/adding_ingredient.js
@@ -1,83 +1,63 @@
-const moveToIngredient = document.querySelectorAll(".move-to-ingredient");
-const moveToSupplier = document.querySelectorAll(".move-to-supplier");
-const moveToAllergy = document.querySelectorAll(".move-to-allergy");
-const nextMoveToSupplier = document.getElementById("move-to-supplier");
-const nextMoveToAllergy = document.getElementById("move-to-allergy");
+const sectionIngredient = document.getElementById("section-ingredient");
+const sectionSupplier = document.getElementById("section-supplier");
+const sectionAllergy = document.getElementById("section-allergy");
 
-let defaultClass =
-  "move-to-supplier px-6 py-2 rounded-lg text-gray-700 hover:text-gray-500 focus:outline-none";
+const navToIngredient = document.getElementById("nav-ingredient");
+const navToSupplier = document.getElementById("nav-supplier");
+const navToAllergy = document.getElementById("nav-allergy");
 
-nextMoveToSupplier.addEventListener("click", (e) => {
-  moveToSupplier.forEach((supplierButton) => {
-    supplierButton.classList.add("bg-white");
-    supplierButton.classList.add("mr-2");
+const nextToSupplier = document.getElementById("next-supplier");
+const nextToAllergy = document.getElementById("next-allergy");
 
-    moveToIngredient.forEach((ingredientButoon) => {
-      ingredientButoon.classList = defaultClass;
-    });
+defaultLinkClass =
+  "px-6 py-2 rounded-lg text-gray-700 hover:text-gray-500 focus:outline-none";
+activeLinkClass =
+  "px-6 py-2 rounded-lg bg-white mr-2 hover:text-gray-500 focus:outline-none";
 
-    moveToAllergy.forEach((allergyButton) => {
-      allergyButton.classList = defaultClass;
-    });
-  });
+navToIngredient.addEventListener("click", () => {
+  navigateToSection(navToIngredient);
 });
 
-nextMoveToAllergy.addEventListener("click", (e) => {
-  moveToAllergy.forEach((allergyButton) => {
-    allergyButton.classList.add("bg-white");
-    allergyButton.classList.add("mr-2");
-
-    moveToIngredient.forEach((ingredientButoon) => {
-      ingredientButoon.classList = defaultClass;
-    });
-
-    moveToSupplier.forEach((supplierButton) => {
-      supplierButton.classList = defaultClass;
-    });
-  });
+navToSupplier.addEventListener("click", () => {
+  navigateToSection(navToSupplier);
 });
 
-moveToIngredient.forEach((ingredientButoon) => {
-  ingredientButoon.addEventListener("click", (e) => {
-    e.target.classList.add("bg-white");
-    e.target.classList.add("mr-2");
-
-    moveToSupplier.forEach((supplierButton) => {
-      supplierButton.classList = defaultClass;
-    });
-
-    moveToAllergy.forEach((allergyButton) => {
-      allergyButton.classList = defaultClass;
-    });
-  });
+navToAllergy.addEventListener("click", () => {
+  navigateToSection(navToAllergy);
 });
 
-moveToSupplier.forEach((supplierButton) => {
-  supplierButton.addEventListener("click", (e) => {
-    e.target.classList.add("bg-white");
-    e.target.classList.add("mr-2");
-
-    moveToIngredient.forEach((ingredientButoon) => {
-      ingredientButoon.classList = defaultClass;
-    });
-
-    moveToAllergy.forEach((allergyButton) => {
-      allergyButton.classList = defaultClass;
-    });
-  });
+nextToSupplier.addEventListener("click", () => {
+  navigateToSection(navToSupplier);
 });
 
-moveToAllergy.forEach((allergyButton) => {
-  allergyButton.addEventListener("click", (e) => {
-    e.target.classList.add("bg-white");
-    e.target.classList.add("mr-2");
-
-    moveToIngredient.forEach((ingredientButoon) => {
-      ingredientButoon.classList = defaultClass;
-    });
-
-    moveToSupplier.forEach((supplierButton) => {
-      supplierButton.classList = defaultClass;
-    });
-  });
+nextToAllergy.addEventListener("click", () => {
+  navigateToSection(navToAllergy);
 });
+
+function navigateToSection(navLinkClicked) {
+  if (navLinkClicked === navToIngredient) {
+    sectionIngredient.classList.remove("hidden");
+    sectionSupplier.classList.add("hidden");
+    sectionAllergy.classList.add("hidden");
+
+    navToIngredient.className = activeLinkClass;
+    navToSupplier.className = defaultLinkClass;
+    navToAllergy.className = defaultLinkClass;
+  } else if (navLinkClicked === navToSupplier) {
+    sectionSupplier.classList.remove("hidden");
+    sectionIngredient.classList.add("hidden");
+    sectionAllergy.classList.add("hidden");
+
+    navToSupplier.className = activeLinkClass;
+    navToIngredient.className = defaultLinkClass;
+    navToAllergy.className = defaultLinkClass;
+  } else {
+    sectionAllergy.classList.remove("hidden");
+    sectionIngredient.classList.add("hidden");
+    sectionSupplier.classList.add("hidden");
+
+    navToAllergy.className = activeLinkClass;
+    navToIngredient.className = defaultLinkClass;
+    navToSupplier.className = defaultLinkClass;
+  }
+}
